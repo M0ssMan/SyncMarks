31e168848d182538b0ef9b4dcdd20c70fa0dd165
Merik Woodmansee <m0ssman@outlook.com>
2017-04-16 19:33:34 -0700

    [CONFIG] added initial config files

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: {
+    "./popup/bundle": "./popup/src/index.js",
+    "./background/bundle": "./background/src/index.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  devtool: 'inline-source-map'
+};
