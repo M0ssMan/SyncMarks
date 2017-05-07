134e8466d390b708ba9fefbb5c75066ecb6fae1d
Merik Woodmansee <m0ssman@outlook.com>
2017-05-06 22:31:22 -0700

    [ADD] profiles flowtype

---
+++ flowtypes/profiles.js
@@ -0,0 +1,12 @@
+// @flow
+
+type profileItem = {
+  text: string
+}
+
+export type profileTypes = {
+  home: Object<profileItem>,
+  laptop: Object<profileItem>,
+  work: Object<profileItem>,
+  mobile: Object<profileItem>
+}
